fd0a8f2a2604e5225a407a868d00c546c2afcc6c
faridguluzade <tu5mhbxzt.code.edu.az>
2023-08-31 16:11:36 -0800

    uploading images to the supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ibuhajbdnmfowdssbwfv.supabase.co";
+export const supabaseUrl = "https://ibuhajbdnmfowdssbwfv.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlidWhhamJkbm1mb3dkc3Nid2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyNTE0NTEsImV4cCI6MjAwODgyNzQ1MX0.4038IQl7Wi8EAvoQ_x-JuZCeeVZUe5YCLz8auocXAas";
 const supabase = createClient(supabaseUrl, supabaseKey);
