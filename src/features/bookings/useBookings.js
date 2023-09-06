5f5c5ab3a58805b897f5f473ff20f64a4adb31bc
faridguluzade <tu5mhbxzt.code.edu.az>
2023-09-05 18:56:56 -0800

    adding server side sorting and filtering in the bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 
 import { getBookings } from "../../services/apiBookings";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading, error };
