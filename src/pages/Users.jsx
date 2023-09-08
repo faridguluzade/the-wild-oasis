796cb3b82f75bf2519f7d9d0572115717eff29c4
faridguluzade <tu5mhbxzt.code.edu.az>
2023-09-07 22:43:07 -0800

    User signup adding & Authorization on Supabase

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,14 @@
 import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import SignupForm from "../features/authentication/SignupForm";
 
 function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <Row>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </Row>
+  );
 }
 
 export default NewUsers;
