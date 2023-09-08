b6961f8b58afca4b48f95e3edaed30207474482b
faridguluzade <tu5mhbxzt.code.edu.az>
2023-09-08 14:55:04 -0800

    implented dark mode with Css variables

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect } from "react";
+
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.remove("light-mode");
+        document.documentElement.classList.add("dark-mode");
+      } else {
+        document.documentElement.classList.remove("dark-mode");
+        document.documentElement.classList.add("light-mode");
+      }
+    },
+    [isDarkMode]
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((dark) => !dark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
