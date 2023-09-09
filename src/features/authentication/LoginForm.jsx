10d1a7bfa194acbd4a82515d589a08f4a22ea35b
faridguluzade <tu5mhbxzt.code.edu.az>
2023-09-09 15:13:35 -0800

    adding a line chart with Rechats library

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("farid@example.com");
-  const [password, setPassword] = useState("Farid1221");
+  const [email, setEmail] = useState("maniyeb962@docwl.com");
+  const [password, setPassword] = useState("12341234");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,45 @@
 import styled from "styled-components";
 
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import { useCabins } from "../cabins/useCabins";
+
+import Stats from "./Stats";
+import Spinner from "../../ui/Spinner";
+import SalesChart from "./SalesChart";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    numDays,
+    isLoading: isLoading2,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Todays activity</div>
+      <div>Chart Stay durations</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
